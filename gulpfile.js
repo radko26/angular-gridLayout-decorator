58f5e34980f2001b0ed05d9b261af4c39ade1a06
Radoslav Petkov <radoslav.petkov@sirma.bg>
2015-02-22 13:58:49 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
 var path = require('path'),
-gulp = require('gulp'),
-uglify = require('gulp-uglify'),
-htmlmin = require('gulp-htmlmin'),
-webserver = require('gulp-webserver'),
-connect = require('gulp-connect'),
-karma = require('gulp-karma'),
-minifyCSS = require('gulp-minify-css'),
-less = require('gulp-less'),
-imagemin = require('gulp-imagemin'),
-clean = require('gulp-clean'),
-jshint = require('gulp-jshint'),
-stylish = require('jshint-stylish'),
-program = require('commander'),
-sonar = require('gulp-sonar'),
-debug = false,
-WATCH_MODE = 'watch',
-RUN_MODE = 'run',
-DIST_PATH = 'public/',
-mode = RUN_MODE;
+  gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  htmlmin = require('gulp-htmlmin'),
+  webserver = require('gulp-webserver'),
+  connect = require('gulp-connect'),
+  karma = require('gulp-karma'),
+  minifyCSS = require('gulp-minify-css'),
+  less = require('gulp-less'),
+  imagemin = require('gulp-imagemin'),
+  clean = require('gulp-clean'),
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  program = require('commander'),
+  sonar = require('gulp-sonar'),
+  debug = false,
+  WATCH_MODE = 'watch',
+  RUN_MODE = 'run',
+  DIST_PATH = 'public/',
+  mode = RUN_MODE;
 
 
 
 program
-.version('0.0.1')
-.option('-d, --dist <value>', 'Specify the output public path')
-.option('-t, --tests [glob]', 'Specify which tests to run')
-.parse(process.argv);
+  .version('0.0.1')
+  .option('-d, --dist <value>', 'Specify the output public path')
+  .option('-t, --tests [glob]', 'Specify which tests to run')
+  .parse(process.argv);
 
-if(program.dist){
-      DIST_PATH = program.dist + DIST_PATH;
+if (program.dist) {
+  DIST_PATH = program.dist + DIST_PATH;
 }
 
 
@@ -38,8 +38,10 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('clean',function(){
-  return gulp.src(DIST_PATH,{read:false}).pipe(clean());
+gulp.task('clean', function() {
+  return gulp.src(DIST_PATH, {
+    read: false
+  }).pipe(clean());
 });
 
 gulp.task('js', function() {
@@ -48,27 +50,29 @@ gulp.task('js', function() {
   if (!debug) {
     jsTask.pipe(uglify());
   }
-  jsTask.pipe(gulp.dest(DIST_PATH +'js'))
-  .pipe(connect.reload());
+  jsTask.pipe(gulp.dest(DIST_PATH + 'js'))
+    .pipe(connect.reload());
 
   jsTask = gulp.src('src/theme/js/**/*.js');
   if (!debug) {
     jsTask.pipe(uglify());
   }
 
-  jsTask.pipe(gulp.dest(DIST_PATH +'theme/js'))
-  .pipe(connect.reload());
+  jsTask.pipe(gulp.dest(DIST_PATH + 'theme/js'))
+    .pipe(connect.reload());
 
 });
 
-gulp.task('lib', function(){
+gulp.task('lib', function() {
   gulp.src('src/lib/**').pipe(gulp.dest(DIST_PATH + '/lib'));
 });
 
-gulp.task('index',function(){
+gulp.task('index', function() {
   var indexTask = gulp.src('src/index.html');
   if (!debug) {
-    indexTask.pipe(htmlmin({ collapseWhitespace: true }));
+    indexTask.pipe(htmlmin({
+      collapseWhitespace: true
+    }));
   }
   indexTask.pipe(gulp.dest(DIST_PATH)).pipe(connect.reload());
 });
@@ -76,10 +80,12 @@ gulp.task('index',function(){
 gulp.task('template', function() {
   var templateTask = gulp.src('src/template/**/*.html');
   if (!debug) {
-    templateTask.pipe(htmlmin({ collapseWhitespace: true }));
+    templateTask.pipe(htmlmin({
+      collapseWhitespace: true
+    }));
   }
-  templateTask.pipe(gulp.dest(DIST_PATH +'template'))
-  .pipe(connect.reload());
+  templateTask.pipe(gulp.dest(DIST_PATH + 'template'))
+    .pipe(connect.reload());
 });
 
 gulp.task('css', function() {
@@ -91,52 +97,51 @@ gulp.task('css', function() {
     options.sourceComments = 'map';
   }
   var cssTask = gulp.src('src/theme/css/less/**/*.less')
-  .pipe(less(options));
+    .pipe(less(options));
   if (!debug) {
     cssTask.pipe(minifyCSS());
   }
-  cssTask.pipe(gulp.dest(DIST_PATH+'theme/css'))
-  .pipe(connect.reload());
+  cssTask.pipe(gulp.dest(DIST_PATH + 'theme/css'))
+    .pipe(connect.reload());
 
-  cssTask = gulp.src('src/theme/css/**/*.css').pipe(gulp.dest(DIST_PATH+'/theme/css')).pipe(connect.reload());
+  cssTask = gulp.src('src/theme/css/**/*.css').pipe(gulp.dest(DIST_PATH + '/theme/css')).pipe(connect.reload());
   if (!debug) {
     cssTask.pipe(minifyCSS());
   }
 
 });
 
-gulp.task('fonts', function(){
-  gulp.src('src/theme/fonts/**/*.*').pipe(gulp.dest(DIST_PATH+'theme/fonts')).pipe(connect.reload());
+gulp.task('fonts', function() {
+  gulp.src('src/theme/fonts/**/*.*').pipe(gulp.dest(DIST_PATH + 'theme/fonts')).pipe(connect.reload());
 });
 
-gulp.task('image', function () {
+gulp.task('image', function() {
   gulp.src('src/theme/images/**/*.*')
-  .pipe(imagemin())
-  .pipe(gulp.dest(DIST_PATH+'theme/images'))
-  .pipe(connect.reload());
+    .pipe(imagemin())
+    .pipe(gulp.dest(DIST_PATH + 'theme/images'))
+    .pipe(connect.reload());
 });
 
 gulp.task('karma', function() {
   return gulp.src(['undefined.js'])
-  .pipe(karma({
-    configFile: 'karma.conf.js',
-    action: mode,
-    tests: program.tests,
-    browsers: ['PhantomJS']
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: mode,
+      tests: program.tests,
+      browsers: ['PhantomJS']
     }))
-  .on('error', function() {});
+    .on('error', function() {});
 });
 
 
-gulp.task('connect', function() { 
+gulp.task('connect', function() {
   gulp.src('.')
     .pipe(webserver({
-      port:8081,
-      proxies: [
-        {
-          source: '/tasks', target: 'http://localhost:8080/SirmaTaskManager/rest/tasks'
-        }
-      ]
+      port: 8081,
+      proxies: [{
+        source: '/tasks',
+        target: 'http://localhost:8080/SirmaTaskManager/rest/tasks'
+      }]
     }));
 
 });
@@ -150,18 +155,18 @@ gulp.task('debug', function() {
 gulp.task('watch-mode', function() {
   mode = WATCH_MODE;
   var jsWatcher = gulp.watch('src/js/**/*.js', ['js']),
-  indexWatcher = gulp.watch('src/index.html',['index']),
-  cssWatcher = gulp.watch('src/theme/less/**/*.less', ['css']),
-  imageWatcher = gulp.watch('src/theme/images/', ['image']),
-  testWatcher = gulp.watch('test/**/*.js', ['karma']),
-  fontsWatcher = gulp.watch('src/theme/fonts/',['fonts']),
-  templateWatcher = gulp.watch('src/template/*.*',['template']);
+    indexWatcher = gulp.watch('src/index.html', ['index']),
+    cssWatcher = gulp.watch('src/theme/less/**/*.less', ['css']),
+    imageWatcher = gulp.watch('src/theme/images/', ['image']),
+    testWatcher = gulp.watch('test/**/*.js', ['karma']),
+    fontsWatcher = gulp.watch('src/theme/fonts/', ['fonts']),
+    templateWatcher = gulp.watch('src/template/*.*', ['template']);
 
   function changeNotification(event) {
     console.log('File', event.path, 'was', event.type, ', running tasks...');
   }
-  indexWatcher.on('change',changeNotification);
-  fontsWatcher.on('change',changeNotification);
+  indexWatcher.on('change', changeNotification);
+  fontsWatcher.on('change', changeNotification);
   jsWatcher.on('change', changeNotification);
   cssWatcher.on('change', changeNotification);
   imageWatcher.on('change', changeNotification);
@@ -170,41 +175,43 @@ gulp.task('watch-mode', function() {
 });
 
 
-gulp.task('sonar', function () {
-    var options = {
-        sonar: {
-            host: {
-                url: 'http://localhost:9000'
-            },
-            jdbc: {
-                url: 'jdbc:h2:tcp://localhost:9092/sonar',
-                username: 'sonar',
-                password: 'sonar'
-            },
-            projectKey: 'sonar:SpaTemplate:1.0.0',
-            projectName: 'Spa template',
-            projectVersion: '1.0.0',
-            sources: 'src/js',
-            language: 'js',
-            sourceEncoding: 'UTF-8',
-            javascript: {
-                lcov: {
-                    reportPath: 'test/sonar_report/lcov.info'
-                }
-            }
+gulp.task('sonar', function() {
+  var options = {
+    sonar: {
+      host: {
+        url: 'http://localhost:9000'
+      },
+      jdbc: {
+        url: 'jdbc:h2:tcp://localhost:9092/sonar',
+        username: 'sonar',
+        password: 'sonar'
+      },
+      projectKey: 'sonar:SpaTemplate:1.0.0',
+      projectName: 'Spa template',
+      projectVersion: '1.0.0',
+      sources: 'src/js',
+      language: 'js',
+      sourceEncoding: 'UTF-8',
+      javascript: {
+        lcov: {
+          reportPath: 'test/sonar_report/lcov.info'
         }
-    };
+      }
+    }
+  };
 
-    return gulp.src('undefined.js', { read: false })
-        .pipe(sonar(options))
-        .on('error', util.log);
+  return gulp.src('undefined.js', {
+      read: false
+    })
+    .pipe(sonar(options))
+    .on('error', util.log);
 });
 
 
 
-gulp.task('build', ['css', 'js','lint','image','template', 'index','lib','fonts']);
+gulp.task('build', ['css', 'js', 'lint', 'image', 'template', 'index', 'lib', 'fonts']);
 gulp.task('all', ['build', 'karma']);
 gulp.task('default', ['watch-mode', 'all']);
 gulp.task('server', ['connect', 'default']);
-gulp.task('test', ['debug','default','connect']);
-gulp.task('start', ['watch-mode','build','connect',]);
+gulp.task('test', ['debug', 'default', 'connect']);
+gulp.task('start', ['watch-mode', 'build', 'connect', ]);
--- src/js/directive/dropdown-directive.js
+++ src/js/directive/dropdown-directive.js
@@ -1,96 +1,105 @@
 define(
-  ['angular'],
-  function(angular) {
-    'use strict';
+    ['angular'],
+    function(angular) {
+        'use strict';
 
-    angular
-    .module('dropdown-directive',[])
-    .directive('dropdownDirective', [ function() {
-     return{
-      scope : {
-       
+        angular
+            .module('dropdown-directive', [])
+            .directive('dropdownDirective', ['$sce', function($sce) {
 
-      },
-      link: function(scope,elem){
-       
-          $('.row').children('.layout-column').each(function(){
-              var children = $(this).children();
-              $(this).empty();
-              $(this).prepend('<div class="column"></div>');
-              $('.column').prepend(children);
-          });
+                function generateDropdownOptionView(option) {
+                    var seed = 142 / 12;
+                    var view = '<div style="border:2px solid #a7a7a7;height:20px;">{content}</div>';
+                    var content = '';
+                    var sizes = option.split(',');
+                    for (var i = 0; i < sizes.length; i++) {
+                        content += '<div style="' + ((i == sizes.length - 1) ? '' : 'border-right:2px solid #a7a7a7;') + 'height:100%;position:relative;float:left;width:' + (sizes[i] * seed) + 'px; "></div>'
+                    }
+                    view = view.replace('{content}', content);
+                    return view;
+                }
 
-          function addSortable(){
-              $(function() {
-                  $( ".column" ).sortable({
-                    connectWith: ".column",
-                    handle: ".portlet-header",
-                    cancel: ".portlet-toggle",
-                    placeholder: "portlet-placeholder ui-corner-all"
-                  });
-                });
-          }
-
-          function changeColumnsSizes(newColumnsSizes){
-              var counter = 0;
-                  $('.row').children('.layout-column').each(function(){
-
-                    var classes = $(this).attr("class").split(' ');
-                    var element = $(this);
-                    $.each(classes, function(i, c) {
-                        if (c.indexOf("col-") == 0) {
-                            element.removeClass(c);
-                        }
+                function addSortable() {
+                    $(function() {
+                        $(".column").sortable({
+                            connectWith: ".column",
+                            handle: ".portlet-header",
+                            cancel: ".portlet-toggle",
+                            placeholder: "portlet-placeholder ui-corner-all"
+                        });
                     });
+                }
 
-                      var newClass = ('col-md-{size} col-xs-{size} col-sm-{size} col-lg-{size}').replace(/{size}/g,newColumnsSizes[counter++]);
-                      $(this).addClass(newClass);
-                      
-                  });
-                
-          }
+                function changeColumnsSizes(newColumnsSizes) {
+                    var counter = 0;
+                    $('.row').children('.layout-column').each(function() {
+                        var classes = $(this).attr("class").split(' ');
+                        var element = $(this);
+                        $.each(classes, function(i, c) {
+                            if (c.indexOf("col-") == 0) {
+                                element.removeClass(c);
+                            }
+                        });
 
-          scope.$watch('option',function(newValue,oldValue){
-              
-              
-              var newColumnsSizes = newValue.split(',');
-              var oldColumnsSizes = oldValue.split(',');
-              
-              if(newColumnsSizes.length < oldColumnsSizes.length){
-                  
-                var extraColumns = oldColumnsSizes.length - newColumnsSizes.length;
-                var elements  = [];
-                 while(extraColumns > 0){
-                    elements.push( $('.layout-column:last-child > .column').children() );
-                    console.log(elements);
-                    $('.layout-column:last-child').remove();
-                    extraColumns--;
-                 } 
-                 $('.layout-column:last-child > .column').append(elements);
-                  changeColumnsSizes(newColumnsSizes);
-              }else if(newColumnsSizes.length > oldColumnsSizes.length){
-                  changeColumnsSizes(newColumnsSizes);
-                  var counter = oldColumnsSizes.length;
-                  while(counter < newColumnsSizes.length ){
-                    var newColumn = ('<div class="layout-column col-md-{size} col-xs-{size} col-sm-{size} col-lg-{size}"><div class="column"></div></div>').replace(/{size}/g,newColumnsSizes[counter++]);
-                    $('.row').append(newColumn);
+                        var newClass = ('col-md-{size} col-xs-{size} col-sm-{size} col-lg-{size}').replace(/{size}/g, newColumnsSizes[counter++]);
+                        $(this).addClass(newClass);
 
-                  }
+                    });
+                }
 
-              }else{
-                changeColumnsSizes(newColumnsSizes);
-
-              }
+                return {
+                    scope: {
 
 
-              addSortable();
+                    },
+                    link: function(scope, elem) {
+                        scope.generateDropdownOptionView = function(option) {
+                            return $sce.trustAsHtml(generateDropdownOptionView(option));
+                        }
 
-          });
+                        $('.row').children('.layout-column').each(function() {
+                            var children = $(this).children();
+                            $(this).empty();
+                            $(this).prepend('<div class="column"></div>');
+                            $('.column').prepend(children);
+                        });
 
-      },
-        templateUrl: "template/dropdown.html"
-     };
-   }])
+                        scope.$watch('option', function(newValue, oldValue) {
 
-  }
+
+                            var newColumnsSizes = newValue.split(',');
+                            var oldColumnsSizes = oldValue.split(',');
+
+                            if (newColumnsSizes.length < oldColumnsSizes.length) {
+
+                                var extraColumns = oldColumnsSizes.length - newColumnsSizes.length;
+                                var elements = [];
+                                while (extraColumns > 0) {
+                                    elements.push($('.layout-column:last-child > .column').children());
+                                    console.log(elements);
+                                    $('.layout-column:last-child').remove();
+                                    extraColumns--;
+                                }
+                                $('.layout-column:last-child > .column').append(elements);
+                                changeColumnsSizes(newColumnsSizes);
+                            } else if (newColumnsSizes.length > oldColumnsSizes.length) {
+                                changeColumnsSizes(newColumnsSizes);
+                                var counter = oldColumnsSizes.length;
+                                while (counter < newColumnsSizes.length) {
+                                    var newColumn = ('<div class="layout-column col-md-{size} col-xs-{size} col-sm-{size} col-lg-{size}"><div class="column"></div></div>').replace(/{size}/g, newColumnsSizes[counter++]);
+                                    $('.row').append(newColumn);
+                                }
+                            } else {
+                                changeColumnsSizes(newColumnsSizes);
+
+                            }
+
+                            addSortable();
+                        });
+
+                    },
+                    templateUrl: "template/dropdown.html"
+                };
+            }])
+    }
 );
